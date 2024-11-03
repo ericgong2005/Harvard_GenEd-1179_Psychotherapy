36479cd9794446cef7aa36c0bff458462bba5016
Eric Gong <ericgong2005@gmail.com>
2024-11-03 00:12:57 -0400

    inital configurable query

--- Query_LLM.py
+++ Query_LLM.py
@@ -1,11 +1,22 @@
 import google.generativeai as genai
 from dotenv import load_dotenv
 import os
+import json
+
+INPUT_FILE = "questions.jsonl"
 
 load_dotenv()
 
 genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
 
+question_list = []
+
+with open(INPUT_FILE, 'r') as file:
+    for line in file:
+        question_list.append(json.loads(line.strip()))
+
 model = genai.GenerativeModel("gemini-1.5-flash")
-response = model.generate_content("Write a story about a magic backpack.")
-print(response.text)
+
+for question in question_list:
+    response = model.generate_content(question["Prompt"])
+    print(response.text)
